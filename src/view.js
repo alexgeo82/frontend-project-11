e62f91617af376a2bfd4d35caa8e5b5f812817c8
Александр Елякин <elyakinan@gmail.com>
2025-07-18 20:11:16 +0300

    same error 403

--- src/view.js
+++ src/view.js
@@ -1,5 +1,6 @@
 import onChange from 'on-change';
 import validateUrl from './validator.js'
+import axios from 'axios'
 
 export default () => {
     const initialState = {
@@ -14,19 +15,27 @@ export default () => {
 
     const form = document.querySelector('form')
     const input = document.getElementById('url-input')
+    const submitButton = document.querySelector('button[type="submit"]')
 
     const watchedState = onChange(initialState, (path, current, previous) => {
 
-        form.addEventListener('submit', async (e) => {
+        submitButton.addEventListener('click', async (e) => {
             e.preventDefault()
             const url = input.value
+            console.log(url)
             try {
                 await validateUrl(url, initialState.feeds)
+                async () => {
+                    const response = await axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+                    console.log(response.status) // код ответа
+                    console.log(response.data) // тело ответа
+                }
                 initialState.feeds.push(url)
                 form.reset()
                 //input.focus()
             } catch (error) {
-                input.classList.add('is-invalid')
+                console.log(error)
+                initialState.form.error = error
             }
         })
     })

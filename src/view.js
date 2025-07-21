3ad22a0bdab5b7c52c9fb457cbe0c2be3a3dc11e
Александр Елякин <elyakinan@gmail.com>
2025-07-21 18:41:29 +0300

    Step 2

--- src/view.js
+++ src/view.js
@@ -17,26 +17,27 @@ export default () => {
     const input = document.getElementById('url-input')
     const submitButton = document.querySelector('button[type="submit"]')
 
-    const watchedState = onChange(initialState, (path, current, previous) => {
+    const watchedState = onChange(initialState, (path, current, previous) => { })
 
-        submitButton.addEventListener('click', async (e) => {
-            e.preventDefault()
-            const url = input.value
-            console.log(url)
-            try {
-                await validateUrl(url, initialState.feeds)
-                async () => {
-                    const response = await axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
-                    console.log(response.status) // код ответа
-                    console.log(response.data) // тело ответа
-                }
-                initialState.feeds.push(url)
-                form.reset()
-                //input.focus()
-            } catch (error) {
-                console.log(error)
-                initialState.form.error = error
-            }
-        })
+    submitButton.addEventListener('click', async (e) => {
+        e.preventDefault()
+        const url = input.value
+        try {
+            await validateUrl(url, watchedState.feeds)
+            axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+                .then(response => {
+                    console.log(response.data)
+                })
+                .catch(error => {
+                    console.error(error)
+                })
+            initialState.feeds.push(url)
+            form.reset()
+            input.focus()
+        } catch (error) {
+            //console.log(error)
+            initialState.form.error = error
+            input.classList.add('is-invalid')
+        }
     })
 }
